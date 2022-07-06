d2b1e47b624c831189eaf01d66895d49be203283
Fock Space <a86gj3sp4@gmail.com>
2022-07-06 13:16:35 +0800

    send location disable respose

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,27 @@
 const socket = io();
 
-
 socket.on("message", (message) => {
   console.log(message);
 });
 
 const form = document.querySelector("#message-form");
+const input = form.querySelector("input");
+const button = form.querySelector("button");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  button.setAttribute("disabled", "disabled");
+
   const message = e.target.elements.message.value;
+
   socket.emit("sendMessage", message, (error) => {
-     if(error) { 
-        return console.log(error);
-     }
-     console.log('Message delivered');
+    button.removeAttribute("disabled");
+    input.value = "";
+    input.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message delivered");
   });
 });
 
@@ -23,13 +31,19 @@ locat.addEventListener("click", () => {
     return alert("Geolocation is not supported by your browser");
   }
 
+  locat.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    }, () => {
-        console.log('Location shared!');
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        locat.removeAttribute("disabled");
+        console.log("Location shared!");
+      }
+    );
   });
 });
-
